be84a9221eb84f5021bea8febe259b9202fb53f7
송승찬 <superfly09@daum.net>
2023-08-07 13:28:19 +0900

    fix: simple-react 불필요 파일 삭제

--- packages/simple_react/src/index.tsx
+++ packages/simple_react/src/index.tsx
@@ -1,6 +1,5 @@
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { render } from "./hooks/useState/useState";
 
 // const root = ReactDOM.createRoot(
@@ -17,4 +16,3 @@ render(App, document.getElementById("root") as HTMLElement);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
